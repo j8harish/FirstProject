225ee3d9146653e06d835004b7ea4a7b6d32f524
Harish Joshi <harishjoshi.1998@gmail.com>
2016-08-28 13:24:15 +0530

    Added password.txt and integrated gitignore

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hellossssssssssss, Worlds This is my first programme!" << std::endl;
+    return 0;
+}
+
+
+// Alt + R to compile and run
+// Alt + T for Terminal
